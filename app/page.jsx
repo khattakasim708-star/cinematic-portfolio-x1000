bd8ba3a2beede56feb12a79f4e40d64ab527eeb8
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 17:20:04 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,13 @@
-// app/page.tsx
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import ThreeBackground from "../components/ThreeBackground";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import ThreeBackground from "./components/ThreeBackground";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <main className="relative w-full h-full min-h-screen overflow-hidden">
+    <main className="relative w-full h-full min-h-screen">
       <ThreeBackground />
       <Navbar />
       <Hero />
