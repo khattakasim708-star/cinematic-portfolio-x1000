a48bd695bacdf5592f9748b9f8a24c792774871d
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 16:18:36 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Cinematic Portfolio X1000",
+  description: "Ultra animated portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
