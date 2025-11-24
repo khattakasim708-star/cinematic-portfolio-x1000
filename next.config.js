069ab5c5d772d292c54bc035fbf3ab89e91ce7b8
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 18:09:58 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
-  // REMOVE experimental.appDir, Next.js 14 uses App Router by default
+  swcMinify: true,
+  output: 'export', // required for static export with Netlify
+  experimental: {
+    appDir: true // optional, Next.js 14 uses App Router by default
+  }
 };
 
 module.exports = nextConfig;
