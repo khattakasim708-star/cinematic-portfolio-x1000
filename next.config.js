8c02f83c415f7ce9d774e8b958c4f5a2b3ae8f56
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 17:23:41 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static export compatible with Netlify
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
