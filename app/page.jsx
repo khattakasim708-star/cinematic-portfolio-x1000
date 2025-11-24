ecba0fe1277f20670b565fc39cd3c7cb7dd1cdcc
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 16:55:07 +0500

    Update and rename page.js to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import ThreeBackground from "./components/ThreeBackground";
+
+// Dynamically import ThreeBackground so it doesn't run on the server
+const ThreeBackground = dynamic(
+  () => import("./components/ThreeBackground"),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
