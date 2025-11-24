66cdbddd0618b57cb24f75c0a18e77e57bd1d84d
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 17:57:00 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  swcMinify: true
+  // REMOVE experimental.appDir, Next.js 14 uses App Router by default
 };
 
 module.exports = nextConfig;
