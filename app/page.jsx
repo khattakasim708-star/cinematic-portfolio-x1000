0ea7afb26dd41b9e477aa8a7ae1af22b22438747
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 17:49:24 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
-"use client";
-
-import Hero from "./components/Hero";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 
 export default function Home() {
   return (
-    <main className="bg-black text-white min-h-screen font-sans">
+    <>
+      <Navbar />
       <Hero />
       <Skills />
       <Projects />
-    </main>
+      <Contact />
+    </>
   );
 }
