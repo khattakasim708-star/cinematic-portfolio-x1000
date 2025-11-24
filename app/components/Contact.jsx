4d33eeaa8ace644a7377f2b5d53b9c654e4bf1d4
khattakasim708-star <khattakasim708@gmail.com>
2025-11-24 16:23:11 +0500

    Create Contact.jsx

---
+++ app/components/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <section className="px-6 py-20">
+      <h2 className="text-4xl glow text-center mb-10">Contact</h2>
+
+      <div className="text-center opacity-90">
+        <p>Email: asimkhattak983@gmail.com</p>
+        <p>WhatsApp: +92 3374817436</p>
+      </div>
+    </section>
+  );
+}
